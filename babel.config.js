801279ad95906fb41d9621689743e9b9982758cf
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2024-11-28 20:30:46 +0100

    Configure babel and TS for module resolution and aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
+require('dotenv').config();
+const countryPath = `./src/countries/${process.env.COUNTRY}/Country`;
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ["module-resolver", {
+      root: ["./src", countryPath, "./src/countries/default"],
+      alias: {
+        "Country": countryPath
+      },
+      extensions: [".js", ".jsx", ".es", ".es6", ".mjs"]
+    }
+  ]
+  ]
 };
